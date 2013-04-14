4706acef6b0abce8b35cbcc113fe6d1db12bf345
Miles Johnson <mileswjohnson@gmail.com>
2013-04-14 16:47:42 -0700

    Added more fixtures

---
+++ src/Titon/Test/Fixture/DispatcherFixture.php
@@ -0,0 +1,19 @@
+<?php
+/**
+ * @copyright	Copyright 2010-2013, The Titon Project
+ * @license		http://opensource.org/licenses/bsd-license.php
+ * @link		http://titon.io
+ */
+
+namespace Titon\Test\Fixture;
+
+use Titon\Mvc\Dispatcher\AbstractDispatcher;
+
+/**
+ * Fixture for Titon\Mvc\Dispatcher.
+ */
+class DispatcherFixture extends AbstractDispatcher {
+
+	public function dispatch() {}
+
+}
---
+++ src/Titon/Test/Fixture/ModuleFixture.php
@@ -0,0 +1,17 @@
+<?php
+/**
+ * @copyright	Copyright 2010-2013, The Titon Project
+ * @license		http://opensource.org/licenses/bsd-license.php
+ * @link		http://titon.io
+ */
+
+namespace Titon\Test\Fixture;
+
+use Titon\Mvc\Module\AbstractModule;
+
+/**
+ * Fixture for Titon\Mvc\Module.
+ */
+class ModuleFixture extends AbstractModule {
+
+}
