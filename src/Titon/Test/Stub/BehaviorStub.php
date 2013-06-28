c59cfbd5a42592e17a8b7e945b99a7f461ea8bef
Miles Johnson <mileswjohnson@gmail.com>
2013-06-28 14:31:51 -0700

    Added BehaviorStub

---
+++ src/Titon/Test/Stub/BehaviorStub.php
@@ -0,0 +1,17 @@
+<?php
+/**
+ * @copyright	Copyright 2010-2013, The Titon Project
+ * @license		http://opensource.org/licenses/bsd-license.php
+ * @link		http://titon.io
+ */
+
+namespace Titon\Test\Stub;
+
+use Titon\Model\Behavior\AbstractBehavior;
+
+/**
+ * Stub for Titon\Model\Behavior.
+ */
+class BehaviorStub extends AbstractBehavior {
+
+}
