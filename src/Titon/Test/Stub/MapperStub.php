8ee4d45bc2f6d9d0c91dfbc0c1005944db431139
Miles Johnson <mileswjohnson@gmail.com>
2014-01-22 23:39:50 -0800

    Added MapperStub

---
+++ src/Titon/Test/Stub/MapperStub.php
@@ -0,0 +1,17 @@
+<?php
+/**
+ * @copyright   2010-2013, The Titon Project
+ * @license     http://opensource.org/licenses/bsd-license.php
+ * @link        http://titon.io
+ */
+
+namespace Titon\Test\Stub;
+
+use Titon\Db\Mapper\AbstractMapper;
+
+/**
+ * Stub for Titon\Db\Mapper.
+ */
+class MapperStub extends AbstractMapper {
+
+}
