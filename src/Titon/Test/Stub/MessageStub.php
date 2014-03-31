5f945f9e91335fa6739f66c7160bb35368dc4d3d
Miles Johnson <mileswjohnson@gmail.com>
2014-03-30 21:22:03 -0700

    Add HTTP message stub

---
+++ src/Titon/Test/Stub/MessageStub.php
@@ -0,0 +1,16 @@
+<?php
+/**
+ * @copyright   2010-2013, The Titon Project
+ * @license     http://opensource.org/licenses/bsd-license.php
+ * @link        http://titon.io
+ */
+
+namespace Titon\Test\Stub;
+
+use Titon\Http\AbstractMessage;
+
+class MessageStub extends AbstractMessage {
+
+    public function getProtocolVersion() {}
+
+}
